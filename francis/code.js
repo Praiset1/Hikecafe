b33ad69708e36bd522510e31e9f715e77f334129
janefrancis2345 <61730151+janefrancis2345@users.noreply.github.com>
2020-04-01 04:21:19 +0300

    Validations

--- francis/code.js
+++ francis/code.js
@@ -1,88 +1,80 @@
-//validate Register Form 
-$("#registerForm").validate({
-rules:{
-        name:{
-            required:true,
-            minlength:3,
-            accept:"[a-zA-Z]+"
+$(document).ready(function () {
+
+    //validate Register Form
+    $("#registerForm").validate({
+
+        rules: {
+            name: {
+                required: true,
+                minlength: 3,
+                accept: "[a-zA-Z]+"
+            },
+
+            email: {
+                required: true,
+            },
+
+            password: {
+                required: true,
+                minlength: 4,
+                maxlength: 20
+            },
+
         },
-     
-     password:{
-         required:true,
-         minlength:5,
-         maxlength:20
-     },
-    
-    email:{
-        required:true,
-        
-    }
-    },
-   messages:{
-       required:"Please Enter Your Name!",
-       minlength:"Your Name must contain letters only!"
-   },
-   password:{
-       required:"Please Provide Your password!",
-       minlength:"Your password must be atleast 5 characters long!"
-   },
-   
-   email:{
-    required:"Please Enter Your email!", 
-    username:"Please Enter valid eamil!"  
-   }
+        messages: {
+            name: {
+                required: "Please Enter Your Name!",
+                minlength: "Your Name must be atleast 3 characters long!",
+                accept: "Your Name must contain letters only!"
+            },
+            password: {
+                required: "Please Provide Your password!",
+                minlength: "Your password must be atleast 4 characters long!"
+            },
+
+            email: {
+                required: "Please Enter Your email!",
+                eamil: "Please Enter valid eamil!"
+            }
+        }
+    });
+    //validate Login Form
+    $("#loginForm").validate({
+        rules: {
+            email: {
+                required: true,
+            },
+            password: {
+                required: true,
+            }
+        },
+        messages: {
+            email: {
+                required: "please Enter Email!",
+                email: "Please Enter valid Email!"
+            },
+            password: {
+                required: "Please provide Your Password!"
+            }
+        }
+    });
 });
 
-//validate Login Form
-$("#loginForm").validate({
-    rules:{
-        email:{
-            required:true,
-            
-            
-
-     },
-     password:{
-         required:true,
-     }
-
-    },
-    messages:{
-    
-email:{
-    required:"please Enter Email!",
-    email:"Please Enter valid Email!"
-},
-    
-    
-
-    
-    
-     password:{
-         required:"Please provide Your Password!"
-     }
-    }
-});
-function myFunction(){
+function myFunction() {
     document.getElementById("myDropdown").classList.toggle(show);
 }
-window.onclick = function(event){
-    if (!event.target.matches('.dropbtn'))
-
-{
-    var myDropdown=
-    document.getElementsByClassName("dropdown-contents");
-    var i;
-    for(i = 0;i < dropdowns.length; i++) 
-    {
-        var openDropdown = dropdowns[i];
-        (openDropdown.classList.contains('show'))
-        {
-openDropdown.classList.remove('show');
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var myDropdown =
+            document.getElementsByClassName("dropdown-contents");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            (openDropdown.classList.contains('show'))
+            {
+                openDropdown.classList.remove('show');
+            }
         }
     }
-}
-
-
 
 }
